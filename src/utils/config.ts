8bd56c4e357544427f07657e73947abd288c8b70
FrancoisToureille <toureillefrancois@gmail.com>
2026-02-03 11:45:14 +0100

    url rabbit maj

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,7 +3,8 @@ import { configType } from '../types/config';
 const config: configType = {
   PORT: Number(process.env.PORT ?? 3006),
   INTERNAL_SECRET: process.env.INTERNAL_SECRET ?? '',
-  RABBITMQ_URL: process.env.RABBITMQ_URL ?? 'amqp://guest:guest@localhost:5672',
+  RABBITMQ_URL:
+    process.env.RABBITMQ_URL ?? 'amqp://guest:guest@rabbitmq_test:5672',
 };
 
 export { config };
